3f82fcceaf59fe79afc5fc9281c03864abe3f5f6
yujiang <yu.jiang.163@gmail.com>
2011-10-28 11:46:42 +0800

    build a envir for test

--- gameVal.lua
+++ gameVal.lua
@@ -2,19 +2,24 @@
 I had wirtten this before£¬but lua only 200 lines, and just one day work
 --]]
 
-g_csv = {}
-g_csv_name = {}
 
-function csv_read_line(line,i)
-	local name,des,func = string.match(line,"(.+)\t(.+)\t(.+)")
-	--print(name,des,func)
+function csv_read_line(line,i,names)
+	local name,chinese,func = string.match(line,"(.-),(.-),(.+)")
+	--print(name,chinese,func)
 	local t = {}
 	t.name = name
-	t.des = des
-	t.funcname = func
+	t.chinese = chinese
+
+	local num = tonumber(func)
+	if(num == nil) then
+		t.funcname = func
+	else
+		t.init_value = num
+	end
+
 	t.fathers = {}
 	t.childs = {}
-	g_csv_name[name] = i
+	names[name] = i
 	return t;
 end
 
@@ -27,26 +32,26 @@ function insert_table_onlyone(t,value)
 	table.insert(t,value)
 end
 
-function csv_set_father(index,father)
-	insert_table_onlyone(g_csv[index].fathers,father)
-	insert_table_onlyone(g_csv[father].childs,index)
+function csv_set_father(csv,index,father)
+	insert_table_onlyone(csv[index].fathers,father)
+	insert_table_onlyone(csv[father].childs,index)
 end
 
-function bind_func(index,t,file)
+function bind_func(csv,index,t,file)
 	--[[if (kind == 2) then return level*1.5 else return level*2 end
 	-->	function (val) if (val[2] == 2) then return val[1]*1.5 else return val[1]*2 end  end--]]
-	if (#t.funcname > 1) then  --not '0'
+	if (t.funcname) then  --not '0'
 		local s = t.funcname
 		for token in string.gmatch(s,"val_([%w_]+)") do
-			local f = g_csv_name[token]
+			local f = csv.csv_name[token]
 			if (f == nil) then
 				error("not find token ["..token.."] at ".. s)
 			end
-			csv_set_father(index,f)
+			csv_set_father(csv,index,f)
 		end
 
 		for _,j in ipairs(t.fathers) do
-			local src = "val_"..g_csv[j].name
+			local src = "val_"..csv[j].name
 			local des = "val["..j.."]"
 			s = string.gsub(s,src,des)
 		end
@@ -57,23 +62,23 @@ function bind_func(index,t,file)
 	end
 end
 
-function csv_remove_child(f,c)
-	for i,j in ipairs(g_csv[f].childs) do
+function csv_remove_child(csv,f,c)
+	for i,j in ipairs(csv[f].childs) do
 		if (j == c) then
-			table.remove(g_csv[f].childs,i)
+			table.remove(csv[f].childs,i)
 			break
 		end
 	end
-	for i,j in ipairs(g_csv[c].fathers) do
+	for i,j in ipairs(csv[c].fathers) do
 		if (j == f) then
-			table.remove(g_csv[c].fathers,i)
+			table.remove(csv[c].fathers,i)
 			break
 		end
 	end
 end
 
-function csv_is_child(f,c)
-	for i,j in ipairs(g_csv[f].childs) do
+function csv_is_child(csv,f,c)
+	for i,j in ipairs(csv[f].childs) do
 		if (j == c) then
 			return true
 		end
@@ -81,58 +86,103 @@ function csv_is_child(f,c)
 	return false
 end
 
-function csv_is_child_recursive(f,c)
-	if (csv_is_child(f,c)) then
+function csv_is_child_recursive(csv,f,c)
+	if (csv_is_child(csv,f,c)) then
 		return true
 	end
-	for _,k in ipairs(g_csv[c].fathers) do
-		if (csv_is_child_recursive(f,k)) then
+	for _,k in ipairs(csv[c].fathers) do
+		if (csv_is_child_recursive(csv,f,k)) then
 			return true;
 		end
 	end
 	return false;
 end
 
-function csv_read(csv)
+function get_father_level(csv,i)
+	local t = csv[i]
+	if (t.fathers == nil or #t.fathers == 0) then
+		return 0
+	end
+	local level = 0
+	for _,f in ipairs(t.fathers) do
+		local l = get_father_level(csv,f)
+		if (l > level) then
+			level = l
+		end
+	end
+	return level + 1
+end
+
+function csv_read(csv_file,csv_name)
+	local firstline = 1
 	local line = 1
-	for l in io.lines(csv) do
-		t = csv_read_line(l,line)
-		table.insert(g_csv,t)
-		--g_csv[line] = t
-		line = line + 1
+	local csv = {}
+	csv.csv_name = {}
+	for l in io.lines(csv_file) do
+		if (firstline == 1) then
+			firstline  = 0
+		else
+			t = csv_read_line(l,line,csv.csv_name)
+			table.insert(csv,t)
+			line = line + 1
+		end
 	end
 
-	local file = io.open("val_func.lua","w")
-	file:write("val_func = {\n")
-	for i,t in ipairs(g_csv) do
-		bind_func(i,t,file)
+	local tempfile = csv_name.."_val_func.lua"
+
+	local file = io.open(tempfile,"w")
+	--print(tempfile,file)
+
+	file:write("--begin val\n")
+	for i,t in ipairs(csv) do
+		--print(i,t.name)
+		file:write("val_"..t.name.." = "..i.."\n")
+	end
+	file:write("--end val\n\n")
+
+	file:write("return {\n")
+	for i,t in ipairs(csv) do
+		bind_func(csv,i,t,file)
 	end
 	file:write("}")
 	file:close()
-	require("val_func")
-	for _,t in ipairs(g_csv) do
-		if (#t.funcname > 1) then  --not '0'
-			t.func = val_func[t.name]
+
+	csv.table_func = dofile(tempfile)
+
+	for _,t in ipairs(csv) do
+		if (t.funcname) then  --not '0'
+			t.func = csv.table_func[t.name]
 			assert(t.func ~= nil)
 		end
 	end
 
 	--remove redundancy relationship£¬a-->b,c b-->c simplfy to: a-->b b-->c so less function call
-	for f,t in ipairs(g_csv) do
-		rm = {}
+	for f,t in ipairs(csv) do
+		local rm = {}
 		for i,j in ipairs(t.childs) do --for all child if any (chlld->father) also is child then remove this child
-			for _,k in ipairs(g_csv[j].fathers) do
-				if csv_is_child_recursive(f,k) then
+			for _,k in ipairs(csv[j].fathers) do
+				if csv_is_child_recursive(csv,f,k) then
 					rm[i] = j
 					break
 				end
 			end
 		end
 		for i,j in pairs(rm) do
-			print(f,t.name,i,j,g_csv[j].name," can reduced!")
-			csv_remove_child(f,j)
+			print(f,t.name,i,j,csv[j].name," can reduced!")
+			csv_remove_child(csv,f,j)
 		end
 	end
+
+	csv.inits = {};
+	for level = 1,#csv do
+		for i,t in ipairs(csv) do
+			if (get_father_level(csv,i) == level) then
+				table.insert(csv.inits,i)
+			end
+		end
+	end
+
+	return csv
 end
 
 function make_table_size(t,size,value)
@@ -141,44 +191,67 @@ function make_table_size(t,size,value)
 	end
 end
 
-function csv_init(person)
-	person.val = {}
-	make_table_size(person.val,#g_csv,0)
+function csv_init(person,csv)
+	local val = {}
+	person.val = val
+	person.val.csv = csv
+	make_table_size(person.val,#csv,0)
+
+	for i,t in ipairs(csv) do
+		if (t.init_value ~= nil and t.init_value ~= 0) then
+			val[i] = t.init_value
+			--csv_change_value(person,i,t.init_value)
+		end
+	end
+
+	for _,i in ipairs(csv.inits) do
+		val[i] = csv[i].func(val)
+	end
+
 end
 
-function update_value(person,index)
-	assert(g_csv[index].func ~= nil)
-	--print(index,g_csv[index].name)
-	person.val[index] = g_csv[index].func(person.val);
-	for _,j in ipairs(g_csv[index].childs) do
-		update_value(person,j)
+function update_value(person,index,father)
+	local csv = person.val.csv
+	assert(csv[index].func ~= nil)
+	local val = person.val
+	val[index] = csv[index].func(val);
+	print(father.. " -> " .. index,csv[index].name.." = "..val[index])
+	for _,j in ipairs(csv[index].childs) do
+		update_value(person,j,index)
 	end
 end
 
 function csv_change_value(person,index,value)
-	assert(g_csv[index].func == nil)
+	local csv = person.val.csv
+	assert(csv[index].func == nil)
 	person.val[index] = value
-	for _,j in ipairs(g_csv[index].childs) do
-		update_value(person,j)
+	print("csv_change_value",index,csv[index].name.." = "..value)
+	for _,j in ipairs(csv[index].childs) do
+		update_value(person,j,index)
 	end
+	print("csv_change_value end",index,value)
 end
 
-function csv_change_value_byname(person,name,value)
-	return csv_change_value(person,g_csv_name[name],value)
+function csv_get_value(person,index)
+	return person.val[index]
 end
 
+function csv_set_value(person,index,value)
+	person.val[index] = value
+end
 
 function csv_print_val(person)
-	print("csv_print_val(person)")
+	print("csv_print_val(person)",person)
+	local csv = person.val.csv
 	for i,j in ipairs(person.val) do
 		if (j ~= 0) then
-			print(g_csv[i].name.." = "..j)
+			print(csv[i].name.."("..csv[i].chinese ..") = "..j)
 		end
 	end
 end
 
-function csv_print()
-	for i,t in ipairs(g_csv) do
+function csv_print(csv)
+	for i,t in ipairs(csv) do
 		s = ""
 		for i,j in ipairs(t.childs) do
 			s = s..j..","
@@ -191,14 +264,3 @@ function csv_print()
 	end
 end
 
-function test()
-	csv_read("test.csv")
-	p = {}
-	csv_init(p)
-	csv_print_val(p)
-	csv_change_value(p,1,10)
-	csv_print_val(p)
-	csv_change_value_byname(p,"level",8)
-	csv_print_val(p)
-end
-test()
